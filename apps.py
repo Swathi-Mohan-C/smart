fb56a63fccf2b1eb3ba29a85a849ebc7190cbcb7
Swathi-Mohan-C <126903415+Swathi-Mohan-C@users.noreply.github.com>
2023-03-09 15:18:32 +0530

    hi

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OnlineshopConfig(AppConfig):
+class SearchappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'onlineshop'
+    name = 'searchapp'
